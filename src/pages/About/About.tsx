01824b2ac91630e3dc18d40bf532203095384369
sharpman13 <sharpman2001@gmail.com>
2023-02-01 08:01:31 +0100

    fix: push for new deploy

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <h1 className={style.text}>About Us</h1>
-      <p>
+      <p className={style.text}>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae enim
         distinctio facilis sequi facere. Minus, ipsum inventore eius, voluptatum
         quos sit, suscipit cumque tempore iusto ad accusamus aperiam itaque
