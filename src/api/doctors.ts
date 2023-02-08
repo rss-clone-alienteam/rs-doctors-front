a5f0373d6c31c55005260b9d5befee746ef32b33
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2023-02-08 14:47:18 +0100

    feat: add search doctors functionality

---
+++ src/api/doctors.ts
@@ -0,0 +1,15 @@
+import { API } from "aws-amplify";
+
+export const getDoctors = async (specialization: string, city: string) => {
+  try {
+    const data = await API.get("rs-doctors-back", "/doctors/get-doctors", {
+      queryStringParameters: {
+        category: specialization,
+        city: city
+      }
+    });
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/components/SectionFindDoctors/SearchButton/SearchButton.tsx
+++ src/components/SectionFindDoctors/SearchButton/SearchButton.tsx
@@ -1,13 +1,19 @@
 import { Button } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import { FC } from "react";
 
-export const SearchButton = () => {
+interface IProps {
+  onClick: () => void
+}
+
+export const SearchButton: FC<IProps> = ({ onClick }) => {
   return (
     <Button
       variant="contained"
       color="success"
       size="large"
       endIcon={<SendIcon />}
+      onClick={onClick}
     >
       Search
     </Button>
