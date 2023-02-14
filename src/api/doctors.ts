066d5a58505d5127d4282cca793b985b5a83d7c7
Stanislav Gutsev <gutsstas@mail.ru>
2023-02-14 14:04:56 +0300

    feat: add function for coords map

--- src/api/doctors.ts
+++ src/api/doctors.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { API } from "aws-amplify";
 
 export interface IDoctor {
@@ -37,4 +38,12 @@ export const getDoctor = async (id?: string): Promise<IDoctor> => {
   const data = API.get("rs-doctors-back", `/doctors/get-doctor/${id}`, {});
   console.log(data);
   return data;
+};
+
+export const getMap = async (address: string) => {
+  const key = "c28eb37a-b0c8-44bd-b589-29629cb89fa2";
+  // const key = "76383793-1243-485a-97a8-100f5e2d036f";
+  const data = await axios.get(`https://geocode-maps.yandex.ru/1.x/?apikey=${key}&geocode=${address}&format=json`);
+  const cords = data.data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(" ").reverse();
+  return cords;
 };
