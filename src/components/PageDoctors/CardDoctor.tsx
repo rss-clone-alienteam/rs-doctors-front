714358dc08c907765e1127d937c4e7f103bdbd9f
Stanislav Gutsev <gutsstas@mail.ru>
2023-02-19 14:29:35 +0300

    feat: add style and schedule on find doctors

--- src/components/PageDoctors/CardDoctor.tsx
+++ src/components/PageDoctors/CardDoctor.tsx
@@ -1,113 +1,105 @@
 import React from "react";
+import style from "./CardDoctor.module.scss";
 import Box from "@mui/material/Box";
-import { Avatar, Card, CardActions, CardContent, CardHeader, Grid, IconButton, Link, Rating, Typography } from "@mui/material";
+import { Avatar, Card, CardActions, CardContent, CardHeader, CircularProgress, Grid, IconButton, Link, Rating, Typography } from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { useNavigate } from "react-router-dom";
 import { IDoctor } from "../../api/doctors";
+import { getSchedule } from "../../api/schedule";
+import { SectionSchedule } from "../SectionSchedule/SectionSchedule";
+import { useQuery } from "react-query";
+import MedicalServicesIcon from "@mui/icons-material/MedicalServices";
 
 interface DoctorProps {
   doctor: IDoctor;
+  coords: [number, number];
 }
 
-export const CardDoctor = ({ doctor }: DoctorProps) => {
+export const CardDoctor = ({ doctor, coords }: DoctorProps) => {
   const navigate = useNavigate();
 
+  const { isLoading, data } = useQuery("schedule", () => getSchedule(doctor.id));
+
   return (
-    <Box display={"flex"}>
-      <Grid container justifyContent="center" alignItems="center" direction="column" spacing={1} sx={{ width: "100%" }}>
-        <Grid item>
-          <Card sx={{ maxWidth: "100%" }}>
-            <CardHeader
-              avatar={
-                <Avatar
-                  alt="complex"
-                  src={doctor.photo || "../../assets/default-avatar.png"}
-                  sx={{ width: 56, height: 56 }}
-                />
-              }
-              sx={{ cursor: "pointer" }}
-              title={`${doctor.nameDoctor} ${doctor.surname}`}
-              subheader={
-                <>
-                  <Typography variant="body2" color="text.secondary">
-                    {doctor.category}
+    <Box className={style.container}>
+      <Grid container>
+        <Grid item container xs={6}>
+          <Grid item>
+            <Card sx={{ boxShadow: "none" }}>
+              <CardHeader
+                avatar={<Avatar alt="complex" src={doctor.photo || "../../assets/default-avatar.png"} sx={{ width: 65, height: 65 }} />}
+                sx={{ cursor: "pointer", fontSize: "31px" }}
+                // title={`${doctor.nameDoctor} ${doctor.surname}`}
+                title={
+                  <Typography variant="body2" color="black" fontSize="17px">
+                    {`${doctor.nameDoctor} ${doctor.surname}`}
                   </Typography>
-                  <Grid container>
-                    <Grid item>
-                      <Rating name="read-only" value={5} readOnly />
+                }
+                subheader={
+                  <>
+                    <Typography variant="body2" color="text.secondary">
+                      {doctor.category}
+                    </Typography>
+                    <Grid container>
+                      <Grid item>
+                        <Rating name="read-only" value={5} readOnly />
+                      </Grid>
+                      <Grid item className={style.avatarFeedback}>
+                        <Box component="span" sx={{ color: "red" }}>
+                          !!!!!Amount
+                        </Box>
+                        Feedback
+                      </Grid>
                     </Grid>
-                    <Grid item>
-                      <Box component="span" sx={{ color: "red" }}>
-                        !!!!!Amount
-                      </Box>
-                      Feedback
+                  </>
+                }
+                onClick={() => navigate(`/doctor/${doctor.id}`)}
+              />
+              <CardContent>
+                <Grid container direction="column" spacing={2}>
+                  <Grid item container direction="row" flexWrap="nowrap">
+                    <Grid item className={style.infoMapIconContainer} mr={1}>
+                      <LocationOnIcon className={style.infoMapIcon} />
+                    </Grid>
+                    <Grid item container direction="column" alignItems="flex-start">
+                      <Grid item container direction="row">
+                        <Grid item>
+                          <Typography className={style.addressJob}>{doctor.address}</Typography>
+                        </Grid>
+                        <Grid item>
+                          {coords && (
+                            <Link href={`https://yandex.ru/maps/?whatshere[point]=${coords[1]},${coords[0]}&whatshere[zoom]=17`} underline="hover">
+                              Map
+                            </Link>
+                          )}
+                        </Grid>
+                      </Grid>
+                      <Grid item>
+                        <Typography variant="body2"> {doctor.experience}</Typography>
+                      </Grid>
                     </Grid>
                   </Grid>
-                </>
-              }
-              onClick={() => navigate(`/doctor/${doctor.id}`)}
-            />
-            <CardContent>
-              <Grid container mb={1} alignItems="center" display={"flex"} flexWrap={"nowrap"}>
-                <Grid item width={"20px"} mr={"5px"}>
-                  <LocationOnIcon sx={{ display: "flex", alignItems: "center" }} />
-                </Grid>
-                <Grid item mr={1}>
-                  <Typography>{doctor.address}</Typography>
-                </Grid>
-                <Grid item>
-                  <Link href="#" underline="hover">
-                    Map
-                  </Link>
-                  {/* <Box
-                    onClick={() => {
-                      console.log("13333");
-                      getMap();
-                    }}
-                  >
-                    Click me
-                  </Box> */}
-                </Grid>
-              </Grid>
 
-              <Grid container mb={1} display={"flex"} flexWrap={"nowrap"}>
-                {/* <Grid item width={"20px"} mr={"5px"}>
-                  <LocationOnIcon sx={{ display: "flex", alignItems: "center" }} />
-                </Grid> */}
-                <Grid item ml={"25px"}>
-                  <Typography variant="body2"> {doctor.experience}</Typography>
+                  <Grid item container direction="row" flexWrap="nowrap">
+                    <Grid item className={style.infoMapIconContainer} mr={1}>
+                      <MedicalServicesIcon className={style.infoMapIcon} />
+                    </Grid>
+                    <Grid item container direction="column" alignItems="flex-start">
+                      <Grid item>
+                        <Typography>{doctor.servicesSector}</Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
                 </Grid>
-              </Grid>
-
-              {/* <Grid container mb={1} alignItems="center">
-                <Grid item xs={0.3}></Grid>
-                <Grid item mr={1} ml={1}>
-                  {doctor.experience}
-                </Grid>
-                <Grid item display={"flex"} justifyContent={"end"}>
-                  {`${doctor.servicesSector} - ${doctor.price}`}
-                </Grid>
-              </Grid> */}
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with
-                the mussels, if you like.
-              </Typography>
-            </CardContent>
-
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">{/* <FavoriteIcon /> */}</IconButton>
-              <IconButton aria-label="share">{/* <ShareIcon /> */}</IconButton>
-            </CardActions>
-          </Card>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Typography color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the
-            mussels, if you like.
-          </Typography>
+        <Grid item xs={6} sx={{ width: "50%", height: "300px", overflow: "scroll", position: "relative" }}>
+          {isLoading && <CircularProgress size={80} sx={{ position: "absolute", top: "40%", left: "45%" }} />}
+          {data !== undefined && <SectionSchedule data={data.schedule} onClick={() => console.log("12300")} key={data.id} />}
         </Grid>
       </Grid>
-      <Box sx={{ width: "50%", height: "300px", backgroundColor: "#d8e0f8" }}></Box>
     </Box>
   );
 };
